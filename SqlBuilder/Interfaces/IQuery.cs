818a27374d19a88319741f30fdea623ac295ee38
Dmitry Koshevoy <dk@nimses.com>
2018-06-02 16:06:31 +0300

    Query interface and realisations

---
+++ SqlBuilder/Interfaces/IQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlBuilder.Interfaces
+{
+
+	public interface IQuery<T>
+	{
+
+		Delete<T> Delete();
+		Insert<T> Insert();
+		Select<T> Select();
+
+	}
+
+}
---
+++ SqlBuilder/Realisations/Queries/Query.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using SqlBuilder.Attributes;
+using SqlBuilder.Interfaces;
+
+namespace SqlBuilder
+{
+
+	public class Query<T> : IQuery<T>
+	{
+
+		public IParameters Parameters { get; set; }
+
+		public Query() : this(SqlBuilder.Parameters)
+		{
+		}
+
+		public Query(IParameters parameters)
+		{
+			this.Parameters = SqlBuilder.Parameters;
+		}
+
+		#region Methods
+
+		public Delete<T> Delete()
+		{
+			return new Delete<T>(this.Parameters);
+		}
+
+		public Insert<T> Insert()
+		{
+			return new Insert<T>(this.Parameters);
+		}
+
+		public Select<T> Select()
+		{
+			return new Select<T>(this.Parameters);
+		}
+
+		#endregion
+
+		#region Static methods
+
+		public static class Factory
+		{
+
+			public static Delete<T> Delete()
+			{
+				return new Delete<T>(SqlBuilder.Parameters);
+			}
+
+			public static Insert<T> Insert()
+			{
+				return new Insert<T>(SqlBuilder.Parameters);
+			}
+
+			public static Select<T> Select()
+			{
+				return new Select<T>(SqlBuilder.Parameters);
+			}
+
+		}
+
+		#endregion
+
+	}
+
+}
