c898669101ade0b7ef31842c9f6cdf0a83d3ea6e
Dmitry Koshevoy <dima@grunger.net>
2018-06-07 00:33:59 +0300

    Compiler static class

---
+++ SqlBuilder/Common/Compiler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlBuilder.Common
+{
+
+	public static class Compiler
+	{
+
+		public static string LastInsertID(Enums.SqlType query)
+		{
+			switch (query)
+			{
+				case Enums.SqlType.MySql:
+					return "SELECT LAST_INSERT_ID()";
+				case Enums.SqlType.PostgreSql:
+					return "RETURNING 1";
+				default:
+				case Enums.SqlType.MsSql:
+					return "SELECT @@IDENTITY";
+			}
+		}
+
+	}
+
+}
